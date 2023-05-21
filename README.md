0b4f1f130c6ad55908e6b25e3a19c21fde1807f9
David Vaquero <pepesan@gmail.com>
2023-05-21 14:20:36 +0200

    - quitados ficheros package-lock.json y el fichero de resultados - Metida dependencia para resultados en formato xUnit XML - Modificado el package.json para incluir la generación del informe XML

---
+++ README.md
@@ -0,0 +1,14 @@
+# Ejemplos de uso de Mocha y Chai
+
+Repositorio de ejemplos de Mocha y Chai
+
+## Instalación de dependencias
+- npm i
+- yarn i
+- ó pnpm i
+## Ejecución de pruebas
+- npm run test
+
+## Ejecución de pruebas con informes
+- npm run test:xunit-report
+- dejará un fichero text-results.xml con los resultados
---
+++ test/retry_test.js
@@ -0,0 +1,29 @@
+const {Builder, By} = require("selenium-webdriver");
+const {assert} = require("chai");
+var expect = require("chai").expect;
+describe('retries', function () {
+    this.timeout(30000)
+    let driver
+    let vars
+    beforeEach(async function() {
+        driver = await new Builder().forBrowser('chrome').build()
+        vars = {}
+    })
+    afterEach(async function() {
+        await driver.quit();
+    })
+    // Retry all tests in this suite up to 4 times
+    this.retries(4);
+
+    beforeEach(function () {
+        driver.get('https://cursosdedesarrollo.com/');
+    });
+
+    it('should succeed on the 3rd try', async function () {
+        // Specify this test to only retry up to 2 times
+        this.retries(2);
+        let textoBoton = await driver.findElement(By.css("button.wp-block-search__button.wp-element-button")).getText();
+        console.log(textoBoton)
+        assert.equal(textoBoton, 'Buscar', "no tiene el texto Buscar");
+    });
+});
